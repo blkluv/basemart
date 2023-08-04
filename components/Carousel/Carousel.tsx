8110d79605a893f88ad87a75fe9bd3ef2f37bfb7
samina <samina@thirdweb.com>
2023-08-04 13:49:12 -0700

    got the carousel to work whoo weee

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,76 +1,114 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-/**
- * Carousel component for nextJS and Tailwind.
- * Using external library react-easy-swipe for swipe gestures on mobile devices (optional)
- *
- * @param images - Array of images with src and alt attributes
- * @returns React component
- */
-
-// const collection = [
-//   {
-//     id: 1,
-//     collectionTitle: "Featured Collection 1",
-//     collectionDescription: "Collection Description 1",
-//     imageLink: "/nft_placeholder_1.png",
-//   },
-//   {
-//     id: 2,
-//     collectionTitle: "Featured Collection 2",
-//     collectionDescription: "Collection Description 2",
-//     imageLink: "/nft_placeholder_2.png",
-//   },
-//   {
-//     id: 3,
-//     collectionTitle: "Featured Collection 3",
-//     collectionDescription: "Collection Description 3",
-//     imageLink: "/nft_placeholder_3.png",
-//   },
-//   {
-//     id: 4,
-//     collectionTitle: "Featured Collection 4",
-//     collectionDescription: "Collection Description 4",
-//     imageLink: "/nft_placeholder_4.png",
-//   },
-// ];
-
-// type CollectionData = {
-//   id: number;
-//   collectionTitle: string;
-//   collectionDescription: string;
-//   imageLink: string;
-// };
-
-// type Props = {
-//   collection: CollectionData;
-// };
+const collections = [
+  {
+    collectionName: "Wild Monkeez",
+    collectionDescription:
+      "Monkeez is a 4k collection of randomly generated NFTs on Base.",
+    collectionImage: "/nft_placeholder.png",
+    collectionLink: "https://twitter.com/monkeez",
+  },
+  {
+    collectionName: "Insane Monkeez",
+    collectionDescription:
+      "Monkeez is a 4k collection of randomly generated NFTs on Base.",
+    collectionImage: "/nft_placeholder.png",
+    collectionLink: "/",
+  },
+  {
+    collectionName: "Whoa Monkeez",
+    collectionDescription:
+      "Monkeez is a 4k collection of randomly generated NFTs on Base.",
+    collectionImage: "/nft_placeholder.png",
+    collectionLink: "/",
+  },
+  {
+    collectionName: "What Monkeez",
+    collectionDescription:
+      "Monkeez is a 4k collection of randomly generated NFTs on Base.",
+    collectionImage: "/nft_placeholder.png",
+    collectionLink: "/",
+  },
+];
 
 export default function Carousel() {
+  const [active, setActive] = useState(0);
+
+  const nextSlide = () => {
+    let newSlide = active === collections.length - 1 ? 0 : active + 1;
+    setActive(newSlide);
+  };
+
+  const prevSlide = () => {
+    let newSlide = active === 0 ? collections.length - 1 : active - 1;
+    setActive(newSlide);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000); // Auto slide every 5 seconds
+    return () => clearInterval(interval);
+  }, [active]);
+
   return (
-    <div className="carousel w-full max-w-7xl h-min md:h-96 flex flex-col-reverse md:flex-row rounded-2xl md:rounded-3xl overflow-hidden">
+    <div className="carousel w-full max-w-7xl h-min md:h-96 flex flex-col-reverse md:flex-row rounded-2xl md:rounded-3xl overflow-hidden relative">
+      <button
+        onClick={prevSlide}
+        className="absolute left-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
+      >
+        {"<"}
+      </button>
+      {/* Text */}
       <div className="left bg-white w-2/5 h-full flex items-left justify-center flex-col px-12 py-4">
         <span className="tag uppercase text-gray-400 text-xs">
-          featured collection
+          Featured Collection
         </span>
         <h2 className="collection-title font-bold text-4xl mb-4">
-          Wild Monkeez
+          {collections[active].collectionName}
         </h2>
         <p className="description text-base leading-6 max-w-xs">
-          Monkeez is a 4k collection of randomly generated NFTs on Base.
+          {collections[active].collectionDescription}
         </p>
-        <button className="view-collection w-max px-8 py-4 bg-black text-white rounded-xl mt-8">
+        <a
+          href={collections[active].collectionLink}
+          className="view-collection w-max px-8 py-4 bg-black text-white rounded-xl mt-8"
+        >
           View Collection
-        </button>
+        </a>
       </div>
+      {/* Image */}
       <div className="right w-3/5 h-full relative">
-        <Image
-          src="/nft_placeholder.png"
-          layout="fill"
-          objectFit="cover"
-          alt="..."
-        />
+        {collections.map((collection, index) => (
+          <Image
+            key={index}
+            src={collection.collectionImage}
+            layout="fill"
+            objectFit="cover"
+            alt={collection.collectionName}
+            style={{
+              opacity: index === active ? 1 : 0,
+              transition: "opacity 1s",
+            }}
+          />
+        ))}
+      </div>
+      <button
+        onClick={nextSlide}
+        className="absolute right-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
+      >
+        {">"}
+      </button>
+      {/* Pagination */}
+      <div className="absolute bottom-0 flex justify-center w-full mt-8">
+        {collections.map((_, index) => (
+          <span
+            key={index}
+            onClick={() => setActive(index)}
+            className={`h-2 w-6 mx-1 rounded-full cursor-pointer ${
+              index === active ? "bg-black" : "bg-gray-400"
+            }`}
+          />
+        ))}
       </div>
     </div>
   );
--- pages/create.tsx
+++ pages/create.tsx
@@ -7,7 +7,6 @@ import {
   useGrantRole,
 } from "@thirdweb-dev/react";
 import React, { FormEvent, useState } from "react";
-import Container from "../components/Container/Container";
 
 export default function Create() {
   const roleToGrant =
@@ -19,29 +18,26 @@ export default function Create() {
     // Get data from the form.
     const data = {
       collectionAddress: (event.target as any).collectionAddress.value,
-      marketplaceName: (event.target as any).marketplaceName.value,
     };
 
-    const sdk = useSDK();
+    // const sdk = useSDK();
 
-    const tx = async function deployContract() {
-      sdk?.deployer.deployMarketplaceV3({
-        name: data.marketplaceName,
-      });
-    };
+    // const tx = async function deployContract() {
+    //   sdk?.deployer.deployMarketplaceV3({
+    //     data.collectionAddress,
+    //   });
+    // };
   };
 
   return (
-    <Container maxWidth="lg">
+    <div>
       <h1>Create your marketplace contract</h1>
       <p>Create a collection first on thirdweb.</p>
       <form onSubmit={handleSubmit}>
         <label className="label">Collection Address</label>
         <input type="text" name="collectionAddress" />
-        <label className="name">Marketplace Name</label>
-        <input type="text" name="marketplaceName" />
         <button>Create Marketplace</button>
       </form>
-    </Container>
+    </div>
   );
 }
